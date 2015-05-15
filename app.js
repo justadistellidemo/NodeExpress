799efdde3ea23da2a7f1e98e08e3992ac469628f
Alex Leventer <alexleventer@gmail.com>
2015-05-15 09:34:05 -0700

    try 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World, this is a sample Express app, showing a test of CodeShip!');
+  res.send('Hello World, this is a sample Express app, showing a test of CodeShip !');
 });
 
 var server = app.listen(3000, function () {
