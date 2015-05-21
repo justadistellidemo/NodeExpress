79a9e0ef0362458914da15e2281b86f7a1024b28
Alex Leventer <alexleventer@gmail.com>
2015-05-21 08:21:31 -0700

    Adding sample html file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('Hello World, this is a sample Express app, showing a test of CodeShip !');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('Magic is happening on port', port);
 });
