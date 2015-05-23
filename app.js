087787694f17a2adce9a269263e1a28a3fb4f525
Alex Leventer <alexleventer@gmail.com>
2015-05-23 11:30:32 -0700

    Ability to catch web hook

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.post('/catch', function(request, response) {
+  var body = request.body;
+  console.log("Received: ", body);
+  response.sendStatus(200)
+});
+
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
