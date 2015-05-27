d3f83eb5adf105e6e5f5eb7e61924e46ca283ea5
Alex Leventer <alexleventer@gmail.com>
2015-05-26 18:15:18 -0700

    Purposely breaking build

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', homeController.index);
+app.get('/', homeController.index, timeController.getDate);
 app.set('view engine', 'jade');
 
 app.post('/catch', function(request, response) {
